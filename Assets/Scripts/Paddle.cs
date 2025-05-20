2938618ecafa456c5dd09e26951bd579ac1f536b
Angel Roberto Andrade <alroc1234@hotmail.com>
2025-05-20 13:56:06 -0700

    paddle moves, ball bounces

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    float movementHorizontal;
+
+    public float maxX = 7.5f;
+    public float speed = 5.0f;
+    void Update()
+    {
+        movementHorizontal = Input.GetAxis("Horizontal");
+        if ((movementHorizontal < 0 && transform.position.x > -maxX) || (movementHorizontal > 0 && transform.position.x < maxX))
+        {
+            transform.position += movementHorizontal * speed * Time.deltaTime * Vector3.right;
+        }
+    }
+}
